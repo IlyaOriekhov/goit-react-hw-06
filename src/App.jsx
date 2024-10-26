4d90be93ad19c19dd72cc13f2a1843b77a57ffa3
Ilya Oriekhov <164690908+IlyaOriekhov@users.noreply.github.com>
2024-10-26 16:14:07 +0300

    add all

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
 import "./App.css";
 
-function App() {}
+import ContactForm from "./components/ContactForm/ContactForm";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactList from "./components/ContactList/ContactList";
+
+function App() {
+  return (
+    <div>
+      <h1 className="title">Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
+}
 
 export default App;
